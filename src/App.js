e440702a2067e425cc2158d0ec7189fd618d40d1
ajurajesh10 <rajeshkumarkrr10@gmail.com>
2024-11-28 10:23:36 +0530

    create contact page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <>
     <Routes>
       <Route path='/' element={<Home />}></Route>
+      <Route path='/contact' element={<Contact />}></Route>
     </Routes>
     </>
 
